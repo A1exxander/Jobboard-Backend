18f7e4cecafada4d967864bfded782801c7b55b6
A1exxander <88210134+A1exxander@users.noreply.github.com>
2023-02-16 02:59:11 -0500

    Delete JBDBManager directory

--- JBDBManager/src/Main.java
+++
@@ -1,8 +0,0 @@
-import DB.DBManager;
-import java.sql.SQLException;
-
-public class Main {
-    public static void main(String[] args) throws SQLException, InterruptedException {
-        DBManager db = DBManager.getInstance();
-    }
-}
